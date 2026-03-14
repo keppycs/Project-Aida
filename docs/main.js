b7667d4d08c1697832a6e29c77b905d92d9d6f03
keppy <shailin.ace@gmail.com>
2026-03-14 01:04:25 +0100

    made player its own js and css

--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,11 @@
 // ── Config ─────────────────────────────────────────────────────────────────────
 const CONFIG = {
-  title:    "2160p60 HDR",
-  av1Dash:  "https://cdn.keppy.dev/file/ProjectAida/2160p60+HDR/AV1/manifest.mpd",
-  av1Hls:   "https://cdn.keppy.dev/file/ProjectAida/2160p60+HDR/AV1/master.m3u8",
+  title: "2160p60 HDR",
+  av1Dash: "https://cdn.keppy.dev/file/ProjectAida/2160p60+HDR/AV1/manifest.mpd",
+  av1Hls: "https://cdn.keppy.dev/file/ProjectAida/2160p60+HDR/AV1/master.m3u8",
   h265Dash: "https://cdn.keppy.dev/file/ProjectAida/2160p60+HDR/H265/manifest.mpd",
-  h265Hls:  "https://cdn.keppy.dev/file/ProjectAida/2160p60+HDR/H265/master.m3u8",
-  isHdr:    true,
+  h265Hls: "https://cdn.keppy.dev/file/ProjectAida/2160p60+HDR/H265/master.m3u8",
+  isHdr: true,
 };
 
 init();
